cc6940c3f0a829e305b054d6fef8a663d43c0620
Raph Gallardo <draphick@gmail.com>
2021-06-01 12:07:25 -0700

    Changing response

--- pyDiscordBot.py
+++ pyDiscordBot.py
@@ -349,13 +349,13 @@ async def on_message(message):
         args = splitspace[1].split(" ", 1)
         if len(args) == 2:
             writerow('now','now',args[0],args[1],None,None,None,None,addtracksheet,"raphtracking")
-            await message.channel.send("New row added!")
+            # await message.channel.send("New row added!")
             # lastrow = getgsheet(addtracksheet,getuser + "tracking",True,True)
             ttype = "\n    Tracking: " + args[0]
             print(ttype)
             tnotes = "\n    Notes: " + args[1]
             print(tnotes)
-            await message.channel.send("Added ---\n```" + ttype + tnotes + "\n```")
+            await message.channel.send("New row added!\n```" + ttype + tnotes + "\n```")
         else:
             writerow('now','now',args[0],None,None,None,None,None,addtracksheet,getuser + "tracking")
             await message.channel.send("New row added!")
@@ -364,7 +364,7 @@ async def on_message(message):
             print(ttype)
             tnotes = "\n    Notes: None"
             print(tnotes)
-            await message.channel.send("Added ---\n```" + ttype + tnotes + "\n```")
+            await message.channel.send("```" + ttype + tnotes + "\n```")
             
     if message.content.lower().startswith('jtrack'):
         getuser = message.author.name.lower()
@@ -372,13 +372,13 @@ async def on_message(message):
         args = splitspace[1].split(" ", 1)
         if len(args) == 2:
             writerow('now','now',args[0],args[1],None,None,None,None,addtracksheet,"jermztracking")
-            await message.channel.send("New row added!")
+            # await message.channel.send("New row added!")
             # lastrow = getgsheet(addtracksheet,getuser + "tracking",True,True)
             ttype = "\n    Tracking: " + args[0]
             print(ttype)
             tnotes = "\n    Notes: " + args[1]
             print(tnotes)
-            await message.channel.send("Added ---\n```" + ttype + tnotes + "\n```")
+            await message.channel.send("New row added! ---\n```" + ttype + tnotes + "\n```")
         else:
             writerow('now','now',args[0],None,None,None,None,None,addtracksheet,getuser + "tracking")
             await message.channel.send("New row added!")
